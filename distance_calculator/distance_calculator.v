4bfe44cb65e72877c2e6be1aeefb9449a10e0243
Sergi Hernandez Gonzalez <sergi@10-192-88-15client.eduroam.upc.edu>
2024-04-26 13:26:39 +0200

    Pipelined distance calculator implementation and testbench

--- distance_calculator/distance_calculator.v
+++ distance_calculator/distance_calculator.v
@@ -1,25 +1,70 @@
-module distance_calculator #(parameter M, N, B)(
-  input wire [B-1:0] training_data [0:M-1][0:N-1],
-  input wire [B-1:0] training_data_type,
-  input wire [B-1:0] input_data [0:M-1][0:N-1],
-  output reg [B-1:0] distance,
-  output reg [B-1:0] data_type
+module distance_calculator #(parameter M, N, W)(
+  input wire clk,
+  input wire rst,
+  input wire ready,
+  input wire [W-1:0] training_data [0:M-1][0:N-1],
+  input wire [W-1:0] training_data_type,
+  input wire [W-1:0] input_data [0:M-1][0:N-1],
+  output reg [W-1:0] distance,
+  output reg [W-1:0] data_type,
+  output reg done
 );
 
-reg [B-1:0] sub, sum;
+parameter IDLE = 0, CALCULATE = 1, DONE = 2;
+reg [1:0] state;
+
+reg [W-1:0] sub, sum;
 integer i,j;
 
-always_comb begin
-  sum = 0;
-  for(i=0; i < M; i=i+1) begin
-    for(j=0; j < N; j=j+1) begin
-      sub = input_data[i][j] - training_data[i][j];
-      sum = sum + sub*sub;
-    end 
+
+always @(posedge clk)
+  begin
+    if (rst) begin
+      distance <= {W{1'b1}}; //max distance possible
+      done <= 0;
+      sum <= 0;
+      sub <= 0;
+      i <= 0;
+      j <= 0;
+
+      state <= IDLE;
+    end else begin
+      case (state)
+        IDLE: begin
+          done <= 0;
+          sub <= 0;
+          sum <= 0;
+
+          if (ready) begin
+            state = CALCULATE;
+          end 
+        end
+        CALCULATE: begin
+          //colums calculation stage
+          for ( j = 0; j < N; j = j + 1) begin
+            sub <= input_data[i][j] - training_data[i][j];
+            sum <= sum + sub*sub;
+          end
+        
+          if (i == M - 1) begin
+            i <= 0;
+
+            state <= DONE;
+            done <= 1;
+            distance <= sum; //square root avoided for its hardware complexity
+
+          end else begin //rows calculation stage
+            i <= i + 1;
+          end
+        end
+        DONE: begin
+          state <= IDLE;
+        end
+     endcase
+    end
   end
-  distance = sum;
-  data_type = training_data_type;
-end
+  
+assign data_type = training_data_type;
 
 endmodule
 
--- distance_calculator/distance_calculator_tb.v
+++ distance_calculator/distance_calculator_tb.v
@@ -1,22 +1,49 @@
-`timescale 1ns/1ps
+`timescale 10ns/1ns
 `include "distance_calculator.v"
 
 module distance_calculator_tb;
 
+reg clk, rst, ready;
 reg [W-1:0] training_data [0:M-1][0:N-1];
 reg [W-1:0] training_data_type;
 reg [W-1:0] input_data [0:M-1][0:N-1];
 wire [W-1:0] distance;
 wire [W-1:0] data_type;
+wire done;
 
 parameter M=2, N=3, W=32;
 
-distance_calculator #(M,N,W) uut(training_data, training_data_type, input_data,distance, data_type);
+distance_calculator #(M,N,W) uut(clk, rst, ready, training_data, training_data_type, input_data,distance, data_type, done);
+
+// clk generation
+initial begin
+  clk = 1;
+  forever #20 clk = ~clk;
+end
+
+// stimuli generation
+initial begin
+  training_data_type = $urandom_range(0,3);
+  
+  for (integer i=0; i<M; i=i+1) begin
+      for (integer j=0; j<N; j=j+1) begin
+        training_data[i][j]=$urandom_range(0,500);
+        input_data[i][j]=$urandom_range(0,500);
+      end
+  end
+end
 
 initial
 begin
   $dumpfile("distance_calculator_tb.vcd");
   $dumpvars;
+
+  clk = 0;
+  forever #20 clk = ~clk; //50MHz clock (20ns period)
+
+  rst = 1; 
+  #5
+  rst = 0;
 
   training_data_type = $urandom_range(0,3);
   
