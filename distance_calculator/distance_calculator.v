4dce9b4e0b4ae878a92c285b81e3bc938d7bf8b5
Sergi Hernandez Gonzalez <sergi@Air-de-Sergi-4.home>
2024-05-17 15:19:31 +0200

    Replace distance_calculator files with pipelined versions

--- distance_calculator/distance_calculator.v
+++ distance_calculator/distance_calculator.v
@@ -1,25 +1,79 @@
-module distance_calculator #(parameter M, N, B)(
-  input wire [B-1:0] training_data [0:M-1][0:N-1],
-  input wire [B-1:0] training_data_type,
-  input wire [B-1:0] input_data [0:M-1][0:N-1],
-  output reg [B-1:0] distance,
-  output reg [B-1:0] data_type
+module distance_calculator #(parameter M, N, W, MAX_ELEMENTS, TYPE_W)(
+  input wire clk,
+  input wire rst,
+  input wire ready,
+  input wire [W-1:0] training_data [0:(M*N)-1],
+  input wire [TYPE_W-1:0] training_data_type,
+  input wire [W-1:0] input_data [0:(M*N)-1],
+  output reg [W-1:0] distance,
+  output reg [TYPE_W-1:0] data_type,
+  output reg done,
+  output reg data_request
 );
 
-reg [B-1:0] sub, sum;
-integer i,j;
+reg [1:0] state; // 0-IDLE, 1-CALCULATE, 2-DONE, 3-REQUEST_DATA
 
-always_comb begin
-  sum = 0;
-  for(i=0; i < M; i=i+1) begin
-    for(j=0; j < N; j=j+1) begin
-      sub = input_data[i][j] - training_data[i][j];
-      sum = sum + sub*sub;
-    end 
+reg [W-1:0] sub, sum;
+integer i, cycle_count;
+
+
+always @(posedge clk)
+  begin
+    if (rst) begin
+      distance <= {1'b0 ,{(W-1){1'b1}}}; //max distance possible (signed)
+      done <= 1'b0;
+      data_request <= 1'b0;
+      sum <= 0;
+      sub <= 0;
+      i <= 0;
+      cycle_count <= 0;
+      state <= 2'b00; //IDLE
+    end else begin
+      case (state)
+        2'b00: begin //IDLE
+          done <= 1'b0;
+          sub <= 0;
+          sum <= 0;
+
+          if (ready) begin
+            state <= 2'b01; //CALCULATE
+          end 
+        end
+        2'b01: begin //CALCULATE
+          if ((i < (M*N)) && (i < MAX_ELEMENTS) && (cycle_count < (M*N))) begin
+            sub = input_data[i] - training_data[i];
+            sum = sum + sub*sub;
+            i <= i + 1;
+            cycle_count <= cycle_count + 1;
+          end else begin
+            i <= 0;
+            if ((MAX_ELEMENTS < (M*N)) && (cycle_count < (M*N))) begin
+              data_request <= 1'b1; //request remaining data
+              state <= 2'b11; //REQUEST_DATA
+            end else begin
+              cycle_count <= 0;
+              done <= 1'b1;
+              distance <= sum; //square root avoided for its hardware complexity
+              state <= 2'b10; //DONE
+            end
+          end
+        end 
+        2'b10: begin //DONE
+          done <= 1'b0;
+          state <= 2'b00; //IDLE
+        end
+        2'b11: begin //REQUEST_DATA
+          data_request <= 1'b0;
+          if (ready) begin
+            state <= 2'b01; //CALCULATE
+          end
+        end
+
+     endcase
+    end
   end
-  distance = sum;
-  data_type = training_data_type;
-end
+  
+assign data_type = training_data_type;
 
 endmodule
 
--- distance_calculator/distance_calculator_tb.v
+++ distance_calculator/distance_calculator_tb.v
@@ -1,36 +1,68 @@
-`timescale 1ns/1ps
+`timescale 10ns/1ns
 `include "distance_calculator.v"
 
 module distance_calculator_tb;
 
-reg [W-1:0] training_data [0:M-1][0:N-1];
-reg [W-1:0] training_data_type;
-reg [W-1:0] input_data [0:M-1][0:N-1];
+reg clk, rst, ready;
+reg [W-1:0] training_data [0:(M*N)-1];
+reg [TYPE_W-1:0] training_data_type;
+reg [W-1:0] input_data [0:(M*N)-1];
 wire [W-1:0] distance;
-wire [W-1:0] data_type;
+wire [TYPE_W-1:0] data_type;
+wire done;
 
-parameter M=2, N=3, W=32;
+parameter M=5, N=10, W=32, MAX_ELEMENTS=30, TYPE_W=2;
 
-distance_calculator #(M,N,W) uut(training_data, training_data_type, input_data,distance, data_type);
+distance_calculator #(M,N,W,MAX_ELEMENTS,TYPE_W) uut(clk, rst, ready, training_data, training_data_type, input_data,distance, data_type, done, data_request);
+
+// clk generation
+initial begin
+  clk = 1;
+  forever #20 clk = ~clk;
+end
+
+// stimuli generation
+initial begin
+  training_data_type = $urandom_range(0,3);
+  
+  for (integer i=0; i<M; i=i+1) begin
+      for (integer j=0; j<N; j=j+1) begin
+        training_data[i*N + j]=$urandom_range(0,500);
+        input_data[i*N + j]=$urandom_range(0,500);
+      end
+  end
+
+  rst = 1;
+  ready = 0;
+  #15 rst = 0;
+  #500 ready = 1;
+  #25 ready = 0;
+
+  #2800
+  training_data_type = $urandom_range(3,6);
+  
+  for (integer i=0; i<M; i=i+1) begin
+      for (integer j=0; j<N; j=j+1) begin
+        training_data[i*N + j]=1;
+        input_data[i*N + j]=0;
+      end
+  end
+
+  ready = 0;
+  #500 ready = 1;
+  #25 ready = 0;
+
+end
 
 initial
 begin
   $dumpfile("distance_calculator_tb.vcd");
   $dumpvars;
 
-  training_data_type = $urandom_range(0,3);
-  
-  for (integer i=0; i<M; i=i+1) begin
-      for (integer j=0; j<N; j=j+1) begin
-        training_data[i][j]=$urandom_range(0,500);
-        input_data[i][j]=$urandom_range(0,500);
-      end
-  end
-
   $display("training_data:");
   for (int i = 0; i < M; i = i + 1) begin
     for (int j = 0; j < N; j = j + 1) begin
-      $display("training_data[%0d][%0d] = %0d", i, j, training_data[i][j]);
+      $display("training_data[%0d][%0d] = %0d", i, j, training_data[i*N+j]);
     end
   end
   $display("Training data type: %0d", training_data_type);
@@ -38,12 +70,19 @@ begin
   $display("input_data:");
   for (int i = 0; i < M; i = i + 1) begin
     for (int j = 0; j < N; j = j + 1) begin
-      $display("input_data[%0d][%0d] = %0d", i, j, input_data[i][j]);
+      $display("input_data[%0d][%0d] = %0d", i, j, input_data[i*N+j]);
     end
   end
-  #10;
+  #2800;
   $display("distance = %0f", distance); 
   $display("Data type = %0d", data_type);
-end 
 
+ #30000;
+  $display("distance = %0f", distance); 
+  $display("Data type = %0d", data_type);
+
+
+  #500
+  $finish;
+end 
 endmodule
