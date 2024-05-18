58eb2f3ae46d406393e57be1edcc7d5a52b986d6
Sergi Hernandez Gonzalez <sergi@Air-de-Sergi-4.home>
2024-05-18 22:17:09 +0200

    Testbench extension for distance calculator

--- distance_calculator/distance_calculator_tb.v
+++ distance_calculator/distance_calculator_tb.v
@@ -11,7 +11,11 @@ wire [W-1:0] distance;
 wire [TYPE_W-1:0] data_type;
 wire done;
 
-parameter M=5, N=10, W=32, MAX_ELEMENTS=30, TYPE_W=2;
+parameter M=60, N=10, W=32, MAX_ELEMENTS=30, TYPE_W=2;
+
+reg [W-1:0] training_data_temp [0:(M*N)-1];
+reg [W-1:0] input_data_temp [0:(M*N)-1];
+integer i,j;
 
 distance_calculator #(M,N,W,MAX_ELEMENTS,TYPE_W) uut(clk, rst, ready, training_data, training_data_type, input_data,distance, data_type, done, data_request);
 
@@ -21,68 +25,158 @@ initial begin
   forever #20 clk = ~clk;
 end
 
-// stimuli generation
-initial begin
+//task definition
+task set_data(input integer range, input data_stream);
   training_data_type = $urandom_range(0,3);
-  
-  for (integer i=0; i<M; i=i+1) begin
-      for (integer j=0; j<N; j=j+1) begin
-        training_data[i*N + j]=$urandom_range(0,500);
-        input_data[i*N + j]=$urandom_range(0,500);
-      end
+  for (integer i=0; i<(M*N); i=i+1) begin
+    training_data_temp[i]=$urandom_range(0,range);
+    input_data_temp[i]=$urandom_range(0,range);
   end
 
-  rst = 1;
-  ready = 0;
-  #15 rst = 0;
-  #500 ready = 1;
-  #25 ready = 0;
-
-  #2800
-  training_data_type = $urandom_range(3,6);
-  
-  for (integer i=0; i<M; i=i+1) begin
-      for (integer j=0; j<N; j=j+1) begin
-        training_data[i*N + j]=1;
-        input_data[i*N + j]=0;
+  if (!data_stream) begin // M*N < MAX_ELEMENTS
+    for (integer i=0; i<(M*N); i=i+1) begin
+      training_data[i]=training_data_temp[i];
+      input_data[i]=input_data_temp[i];
+    end
+    ready = 1'b1;
+    @(posedge clk);
+    #1;
+    ready = 1'b0;
+    wait(done);
+    @(posedge clk);
+    #1;
+  end else begin
+    i = 0;
+    j = 0;
+    while (i < (M*N)) begin
+      training_data[j] = training_data_temp[i];
+      input_data[j] = input_data_temp[i];
+      i = i + 1;
+      if (j < MAX_ELEMENTS) begin 
+        j = j + 1;
+      end else begin // data burst
+        j = 0;
+        ready = 1'b1;
+        @(posedge clk);
+        #1;
+        ready = 1'b0;
+        wait(data_request);
+        @(posedge clk);
+        #1;
       end
+    end
+
+    ready = 1'b1;
+    @(posedge clk);
+    #1;
+    ready = 1'b0;
+    wait(done);
+    @(posedge clk);
+    #1;
+  end
+endtask
+
+task simple_data_test(input data_stream);
+  training_data_type = $urandom_range(0,3);
+  for (integer i=0; i<(M*N); i=i+1) begin
+    training_data_temp[i]=1;
+    input_data_temp[i]=0;
   end
 
-  ready = 0;
-  #500 ready = 1;
-  #25 ready = 0;
+  if (!data_stream) begin // M*N < MAX_ELEMENTS
+    for (integer i=0; i<(M*N); i=i+1) begin
+      training_data[i]=training_data_temp[i];
+      input_data[i]=input_data_temp[i];
+    end
+    ready = 1'b1;
+    @(posedge clk);
+    #1;
+    ready = 1'b0;
+    wait(done);
+    @(posedge clk);
+    #1;
+  end else begin
+    i = 0;
+    j = 0;
+    while (i < (M*N)) begin
+      training_data[j] = training_data_temp[i];
+      input_data[j] = input_data_temp[i];
+      i = i + 1;
+      if (j < MAX_ELEMENTS) begin 
+        j = j + 1;
+      end else begin // data burst
+        j = 0;
+        ready = 1'b1;
+        @(posedge clk);
+        #1;
+        ready = 1'b0;
+        wait(data_request);
+        @(posedge clk);
+        #1;
+      end
+    end
 
-end
+    ready = 1'b1;
+    @(posedge clk);
+    #1;
+    ready = 1'b0;
+    wait(done);
+    @(posedge clk);
+    #1;
+  end
+endtask
 
-initial
-begin
-  $dumpfile("distance_calculator_tb.vcd");
-  $dumpvars;
 
-  $display("training_data:");
+task display_data();
+  wait(ready);
+  $display("Training data:");
   for (int i = 0; i < M; i = i + 1) begin
     for (int j = 0; j < N; j = j + 1) begin
-      $display("training_data[%0d][%0d] = %0d", i, j, training_data[i*N+j]);
+      $display("training_data[%0d][%0d] = %0d", i, j, training_data_temp[i*N+j]);
     end
   end
   $display("Training data type: %0d", training_data_type);
 
-  $display("input_data:");
+  $display("Input data:");
   for (int i = 0; i < M; i = i + 1) begin
     for (int j = 0; j < N; j = j + 1) begin
-      $display("input_data[%0d][%0d] = %0d", i, j, input_data[i*N+j]);
+      $display("input_data[%0d][%0d] = %0d", i, j, input_data_temp[i*N+j]);
     end
   end
-  #2800;
-  $display("distance = %0f", distance); 
-  $display("Data type = %0d", data_type);
+  wait(done);
+  $display("distance = %0d", distance); 
+endtask
 
- #30000;
-  $display("distance = %0f", distance); 
-  $display("Data type = %0d", data_type);
+// stimuli generation
+initial begin
+  rst = 1'b1;
+  ready = 1'b0;
+  #5 rst = 1'b0;
+  @(posedge clk);
+  
+  if ((M*N) < MAX_ELEMENTS) begin
+    set_data(100,0);
+    set_data(200,0);
+    set_data(300,0);
+    simple_data_test(0);
+  end else begin
+    set_data(100,1);
+    set_data(200,1);
+    set_data(300,1);
+    simple_data_test(1);
+  end
+end
 
+initial begin
+  $dumpfile("distance_calculator_tb.vcd");
+  $dumpvars;
 
-  #500
+  display_data();
+  display_data();
+  display_data();
+  display_data();
+
+  #20000;
   $finish;
 end 
 endmodule
