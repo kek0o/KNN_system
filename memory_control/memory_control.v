ea453ff19342fac12cd90482bac65c39cc398e6d
Sergi Hernandez Gonzalez <sergi@MacBook-Air-de-Sergi-4.local>
2024-05-25 17:54:02 +0200

    Corrected Memory control module and optimized testbench

--- memory_control/memory_control.v
+++ memory_control/memory_control.v
@@ -1,11 +1,10 @@
-module memory_control #(parameter M, N, W, MAX_ELEMENTS, ADDR_W, BASE_T_ADDR, BASE_I_ADDR)(
+module memory_control #(parameter M, N, W, MAX_ELEMENTS, TYPE_W, L, ADDR_W, BASE_T_ADDR, BASE_I_ADDR)(
   input wire clk,
   input wire rst,
   input wire start,
   input wire data_request,
   input wire done,
-  input wire done_calc,
-  input wire [W-1:0] inferred_type,
+  input wire [TYPE_W-1:0] inferred_type,
   input wire inference_done,
   input wire [W-1:0] readdata,
   output reg read,
@@ -15,7 +14,7 @@ module memory_control #(parameter M, N, W, MAX_ELEMENTS, ADDR_W, BASE_T_ADDR, BA
   output reg [ADDR_W-1:0] writeaddress,
   output reg [W-1:0] input_data [0:(M*N)-1],
   output reg [W-1:0] training_data [0:(M*N)-1],
-  output reg [W-1:0] training_data_type,
+  output reg [TYPE_W-1:0] training_data_type,
   output reg read_done
 );
 
@@ -27,12 +26,12 @@ reg latch_type, latch_input, latch_input_done;
 reg [ADDR_W-1:0] training_addr;
 reg [ADDR_W-1:0] input_addr;
 reg [ADDR_W-1:0] inference_addr;
-integer i,j, cycle_count_i, cycle_count_t;
+integer i,j, cycle_count_i, cycle_count_t, done_count;
 reg integer data_limit;
 reg new_request;
 
 assign data_limit = (MAX_ELEMENTS < (M*N)) ? MAX_ELEMENTS : (M*N);
-assign new_request = done | data_request;
+assign new_request = done || data_request;
 
 always@(posedge clk)
 begin 
@@ -43,14 +42,15 @@ begin
     read_done <= 1'b0;
     latch_input_done <= 1'b0;
     training_addr <= BASE_T_ADDR;
-    input_addr <= BASE_I_ADDR;
+    input_addr <= BASE_I_ADDR + W;
     inference_addr <= BASE_I_ADDR;
     write_count <= 5'b0;
     cycle_count_i <= 0;
     cycle_count_t <= 0;
+    done_count <= 0;
     latch_type <= 1'b1;
     latch_input <= 1'b0;
-    training_data_type <= 0;
+    training_data_type <= {TYPE_W{1'b0}};
     writeaddress <= BASE_I_ADDR;
     readaddress <= BASE_T_ADDR;
     for(i = 0; i < (M*N); i = i + 1) begin
@@ -64,13 +64,13 @@ begin
 	  	4'd0: begin // IDLE
 	  	  read <= 1'b0;
         write <= 1'b0;
-        writedata <= {W{1'b0}};
         read_done <= 1'b0;
         latch_input_done <= 1'b0;
         readaddress <= training_addr;
         write_count <= 5'b0;
         cycle_count_i <= 0;
         cycle_count_t <= 0;
+        done_count <= 0;
         latch_type <= 1'b1;
         for(i = 0; i < (M*N); i = i + 1) begin
           training_data_reg[i] <= {W{1'b0}};
@@ -82,6 +82,9 @@ begin
 	  	4'd1: begin // read data 
 	  		read <= 1'b1;
         if (!write_count[3]) write_count <= write_count + 1'b1;
+
+        if (latch_input) readaddress <= input_addr;
+        else readaddress <= training_addr;
         state <= 4'd2;
       end
       4'd2: begin
@@ -90,42 +93,32 @@ begin
         else state <= latch_input ? 4'd4 : 4'd5;
       end
       4'd3: begin // latch training type
-        training_data_type <= readdata;
+        training_data_type <= readdata[TYPE_W-1:0];
         training_addr <= training_addr + W;
         latch_type <= 1'b0;
-        if (latch_input_done) readaddress <= training_addr + W;
-        else begin
-          readaddress <= input_addr + W; // first address (input_addr) is inferred input data type
-          latch_input <= 1'b1;
-        end
+        if (latch_input_done) latch_input <= 1'b0;
+        else latch_input <= 1'b1;
         state <= 4'd1;
       end
       4'd4: begin // latch input data
-        input_data_reg[j] <= readdata;
         if ((j < data_limit) && (cycle_count_i < (M*N))) begin
-          j <= j + 1;
-          readaddress <= readaddress + W;
+          input_data_reg[j] <= readdata;
           input_addr <= input_addr + W;
+          j <= j + 1;
           cycle_count_i <= cycle_count_i + 1;
         end else begin
           j <= 0;
           latch_input <= 1'b0;
-          readaddress <= training_addr;
-          input_addr <= input_addr + W;
-          if (cycle_count_i == (M*N)) begin
-            cycle_count_i <= 0;
-            input_addr <= input_addr + 2*W; //skip input type
-          end
+          if (cycle_count_i == (M*N)) cycle_count_i <= 0;
         end
         state <= 4'd1;
       end
       4'd5: begin // latch training data
-        training_data_reg[j] <= readdata;
-        training_addr <= training_addr + W;
         if ((j < data_limit) && (cycle_count_t < (M*N))) begin
+          training_data_reg[j] <= readdata;
+          training_addr <= training_addr + W;
           j <= j + 1;
           cycle_count_t <= cycle_count_t + 1;
-          readaddress <= readaddress + W;
           state <= 4'd1;
         end else begin
           j <= 0;
@@ -143,31 +136,31 @@ begin
       end
       4'd7: begin // wait calculation
         read_done <= 1'b0;
-        if (done_calc) state <= 4'd8;
-        else begin
+        if (done) done_count = done_count + 1;
+
+        if (done_count < L) begin
           if (new_request) begin
-            if (done) begin
-              if (data_limit < (M*N)) begin
-                latch_input <= 1'b1;
-                readaddress <= input_addr;
-              end else begin
-                latch_type <= 1'b1;
-                latch_input_done <= 1'b1;
-              end
-            end else begin // data request
-              latch_input <= 1'b1;
-              readaddress <= input_addr;
+            if (data_request) latch_input <= 1'b1;
+            else begin
+              if (data_limit < (M*N)) input_addr <= inference_addr + W;
+              else latch_input_done <= 1'b1;
+              latch_type <= 1'b1;
             end
             state <= 4'd1;
           end else state <= 4'd7;
+        end else begin
+          done_count <= 0;
+          state <= 4'd8;
         end
       end
       4'd8: begin // wait inference
         latch_input_done <= 1'b0;
         if (inference_done) begin
           writeaddress <= inference_addr;
-          writedata <= inferred_type;
+          writedata <= {{(W-TYPE_W){1'b0}},inferred_type};
           inference_addr <= input_addr; // next inference address
+          input_addr <= input_addr + W; // skip input type
+          training_addr <= BASE_T_ADDR; // start reading training data again
           state <= 4'd9;
         end else state <= 4'd8;
       end
--- memory_control/memory_control_tb.v
+++ memory_control/memory_control_tb.v
@@ -2,8 +2,8 @@
 
 module memory_control_tb;
 
-reg clk, rst, start, data_request, done, done_calc;
-reg [W-1:0] inferred_type;
+reg clk, rst, start, data_request, done;
+reg [TYPE_W-1:0] inferred_type;
 reg inference_done;
 reg [W-1:0] readdata;
 wire read;
@@ -13,27 +13,29 @@ wire write;
 wire [ADDR_W-1:0] writeaddress;
 wire [W-1:0] input_data [0:(M*N)-1];
 wire [W-1:0] training_data [0:(M*N)-1];
-wire [W-1:0] training_data_type;
+wire [TYPE_W-1:0] training_data_type;
 wire read_done;
+wire done_calc;
 
 parameter L=64; // number of training matrices
-parameter K=7; // number of neighbours
-parameter M=6, N=10, W=16, MAX_ELEMENTS=32, ADDR_W=25, BASE_T_ADDR=0, BASE_I_ADDR= W*M*N*L;
+parameter K=15; // number of neighbours
+parameter M=6, N=10, W=16, TYPE_W = 3, MAX_ELEMENTS=32, ADDR_W=25, BASE_T_ADDR=0;
+parameter BASE_I_ADDR= W*M*N*L+W*L;
 
-reg [W*M*N*(L+10)-1:0] sdram;
+
+reg [W*M*N*(L+10) + W*(L+10)-1:0] sdram;
 reg set_type;
 reg [W-1:0] matrix_value;
-integer i,j, training_elements, data_elements;
+integer i,j, training_elements;
 integer clk_count;
 integer inference_count;
-reg rw_sdram;
 
 // module instances
-memory_control #(M,N,W,MAX_ELEMENTS,ADDR_W,BASE_T_ADDR,BASE_I_ADDR) mem_ctrl 
-(clk, rst, start, data_request, done, done_calc, inferred_type, inference_done, readdata,
+memory_control #(M,N,W,MAX_ELEMENTS,TYPE_W,L,ADDR_W,BASE_T_ADDR,BASE_I_ADDR) mem_ctrl 
+(clk, rst, start, data_request, done, inferred_type, inference_done, readdata,
 read, readaddress, writedata, write, writeaddress, input_data, training_data, training_data_type, read_done);
 
-knn_system #(M,N,W,MAX_ELEMENTS,W,K,L) knn 
+knn_system #(M,N,W,MAX_ELEMENTS,TYPE_W,K,L) knn 
 (clk, rst, read_done, training_data, training_data_type, input_data, data_request, done, done_calc, inferred_type, inference_done);
 
 // clk generation
@@ -44,40 +46,36 @@ end
 
 //task definition
 task set_sdram_data();
-  set_type = 1'b1;
   i = 0;
   training_elements = 0;
-  while (i < W*M*N*(L+10)) begin
-    matrix_value = $urandom_range(0, (1<<W)-1);
-    if (set_type) begin
-      // set type at the first address of each training matrix
-      if (training_elements < L) begin
-        if (matrix_value < ((1<<W)/5)) sdram[i +:W] = 5;
-        else if (matrix_value < ((1<<W)/4)) sdram[i +:W] = 4;
-        else if (matrix_value < ((1<<W)/3)) sdram[i +:W] = 3;
-        else if (matrix_value < ((1<<W)/2)) sdram[i +:W] = 2;
-        else sdram[i +:W] = 1;
-      end else sdram[i +:W] = 0; // no specific type for input data
+  while (i < (W*M*N*(L+10)+W*(L+10))) begin
+    matrix_value = $urandom_range(0, 100);
+    // set type at the first address of each matrix
+    if (training_elements < L) begin
+      if (matrix_value < 20) sdram[i +:W] = 1;
+      else if (matrix_value < 40) sdram[i +:W] = 2;
+      else if (matrix_value < 60) sdram[i +:W] = 3;
+      else if (matrix_value < 80) sdram[i +:W] = 4;
+      else sdram[i +:W] = 5;
+    end else sdram[i +:W] = 0; // no specific type for input data
+    i = i + W;
 
-      set_type = 1'b0;
-      i = i + W;
-    end else begin
-        for (j = 0; j < (M*N); j = j + 1) sdram[(i + j*W) +:W] = matrix_value;
-        i = i + W*M*N;
-        set_type = 1'b1;
-        training_elements = training_elements + 1;
-    end
+    for (j = 0; j < (M*N); j = j + 1) sdram[(i + j*W) +:W] = matrix_value;
+      i = i + W*M*N;
+      training_elements = training_elements + 1;
   end
   #1;
 endtask
 
 task read_sdram();
+  @(posedge clk);
   readdata = sdram[readaddress +:W];
   #1;
 endtask
 
 task write_sdram();
   clk_count = 0;
+  @(posedge clk);
   sdram[writeaddress +:W] = writedata;
   while (clk_count < 8) begin // simulate writting process
     @(posedge clk);
@@ -88,11 +86,9 @@ endtask
 
 task display_sdram_data();
   i = 0;
-  clk_count = 0;
-  data_elements = 0;
-  while (data_elements < (L + 10) - 1) begin
+  while (i < (W*M*N*(L+10)+W*(L+10))) begin
     $display("Address %0d:", i);
-    if (data_elements < L) begin
+    if (i < BASE_I_ADDR) begin
       $display("Training Type: %0d", sdram[i +: W]);
       i = i + W;
       $display("Training Matrix value: %0d", sdram[i +: W]);
@@ -101,11 +97,10 @@ task display_sdram_data();
       i = i + W;
       $display("Input Matrix value: %0d", sdram[i +: W]);
     end
-    data_elements = data_elements + 1;
      i = i + W*M*N;
   end
-
 endtask
+
 
 // stimuli generation
 initial begin
@@ -116,34 +111,31 @@ initial begin
   #100 start = 1'b1;
   @(posedge clk);
   #1;
-  start = 1'b0;
 end
 
-assign rw_sdram = read | write;
-always @(rw_sdram) begin
-  if (read) begin
-    read_sdram();
-    #1;
-    $display("%0d", readdata);
-  end else begin
-    write_sdram();
-  end
-end
+always @(posedge read) read_sdram();
+always @(posedge write) write_sdram();
+always @(posedge done_calc) $display("Done calculating data set");
 
-always @(posedge clk) if (inference_done) inference_count <= inference_count + 1;
+
+always @(posedge inference_done) begin
+  inference_count = inference_count + 1;
+  if (inference_count == 10) start = 1'b0;
+  $display ("Inference!");
+  $display("Inference number: %0d", inference_count);
+end
 
 initial begin
   $dumpfile("memory_control_tb.vcd");
   $dumpvars;
   inference_count = 0;
+  clk_count = 0;
   set_sdram_data();
   display_sdram_data();
 
-  if (inference_count == 9) begin
-    @(posedge clk);
-      display_sdram_data();
-      #100; 
-      $finish;
-  end
+  #15000000;
+  display_sdram_data();
+  #100; 
+  $finish;
 end 
 endmodule
