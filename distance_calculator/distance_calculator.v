dc38b93a208ab94c0de7ca56532835418240594b
Sergi Hernandez Gonzalez <sergi@10-192-213-242client.eduroam.upc.edu>
2024-06-03 12:22:20 +0200

    Distance calculator port modification

--- distance_calculator/distance_calculator.v
+++ distance_calculator/distance_calculator.v
@@ -11,7 +11,7 @@ module distance_calculator #(parameter M, N, W, MAX_ELEMENTS, TYPE_W)(
   output reg data_request
 );
 
-reg [1:0] state; // 0-IDLE, 1-CALCULATE, 2-DONE, 3-REQUEST_DATA
+reg [2:0] state; // 0-IDLE, 1-CALCULATE, 2-DONE, 3-REQUEST_DATA
 
 reg [W-1:0] sub, sum;
 integer i, cycle_count;
@@ -25,42 +25,46 @@ always @(posedge clk) begin
     sub <= 0;
     i <= 0;
     cycle_count <= 0;
+    data_type <= {TYPE_W{1'b0}};
     state <= 2'b00; //IDLE
   end else begin
     case (state)
-      2'b00: begin //IDLE
+      3'b000: begin //IDLE
         done <= 1'b0;
         sub <= 0;
         sum <= 0;
-        state <= ready ? 2'b01 : 2'b00;
+        state <= ready ? 3'b001 : 3'b000;
       end
-      2'b01: begin //CALCULATE
+      3'b001: begin //CALCULATE
         if ((i < MAX_ELEMENTS) && (cycle_count < (M*N))) begin
-          sub = input_data[(i+1)*W-1 -: W] - training_data[(i+1)*W-1 -: W];
-          sum = sum + sub*sub;
+          sub <= input_data[(i+1)*W-1 -: W] - training_data[(i+1)*W-1 -: W];
           i <= i + 1;
           cycle_count <= cycle_count + 1;
+          state <= 3'b010;
         end else begin
           i <= 0;
           if (cycle_count < (M*N)) begin
             data_request <= 1'b1; //request remaining data
-            state <= 2'b11; //REQUEST_DATA
+            state <= 3'b100; //REQUEST_DATA
           end else begin
             cycle_count <= 0;
-            done <= 1'b1;
-            distance <= sum; //square root avoided for its hardware complexity
-	    data_type <= training_data_type;
-            state <= 2'b10; //DONE
+	          data_type <= training_data_type;
+            state <= 3'b011; //DONE
           end
         end
       end 
-      2'b10: begin //DONE
-        done <= 1'b0;
-        state <= 2'b00; //IDLE
+      3'b010: begin // SUM
+        sum <= sum + sub*sub;
+        state <= 3'b001;
       end
-      2'b11: begin //REQUEST_DATA
+      3'b011: begin //DONE
+        done <= 1'b1;
+        distance <= sum; //square root avoided for its hardware complexity
+        state <= 3'b000; //IDLE
+      end
+      3'b100: begin //REQUEST_DATA
         data_request <= 1'b0;
-        state <= ready ? 2'b01 : 2'b11;
+        state <= ready ? 3'b001 : 3'b100;
       end
     endcase
   end
