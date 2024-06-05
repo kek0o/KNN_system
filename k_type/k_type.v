60f37ce628374cb14b1a3c71487b48d7b7ee4ddb
Sergi Hernandez Gonzalez <sergi@10-192-93-153client.eduroam.upc.edu>
2024-06-05 18:59:11 +0200

    Module with packed arrays

--- k_type/k_type.v
+++ k_type/k_type.v
@@ -9,9 +9,10 @@ module k_type #(K, TYPE_W)(
 
   reg [1:0] state;
   reg stop;
-  integer count [0:(1 << TYPE_W)-1]; // 2^TYPE_W
+  reg [32*(1<<TYPE_W)-1:0] count; // 2^TYPE_W
   integer max_type;
   integer i, j;
+  reg [TYPE_W-1:0] index;
 
   always @(posedge clk) begin
     if (rst) begin
@@ -19,25 +20,28 @@ module k_type #(K, TYPE_W)(
       stop <= 1'b0;
       inferred_type <= {TYPE_W{1'b0}};
       max_type <= 0;
-
-      for (i = 0; i < (1 << TYPE_W); i = i + 1) count[i] <= 0;
+      count <= {32*(1<<TYPE_W){1'b0}};
+      index <= {TYPE_W{1'b0}};
       j <= 0;
-
       state <= 2'b00;
     end else begin
       case (state)
         2'b00: begin // IDLE
           inference_done <= 1'b0;
           max_type <= 0;
-          for (i = 0; i < (1 << TYPE_W); i = i + 1) count[i] <= 0;
+          count <= {32*(1<<TYPE_W){1'b0}};
+          index <= {TYPE_W{1'b0}};
           j <= 0;
-
-          state <= valid_sort ? 2'b01 : 2'b00;
+          if (valid_sort) begin
+            index <= k_nearest_neighbours_type[(j+1)*TYPE_W-1-:TYPE_W];
+            state <= 2'b01;
+          end
         end
         2'b01: begin // INFER
           if (!stop) begin
+            count[32*(index+1)-1-:32] <= count[32*(index+1)-1-:32] + 1;
+            index <= k_nearest_neighbours_type[(j+1)*TYPE_W-1-:TYPE_W];
             if (j < K) begin
-              count[k_nearest_neighbours_type[(j+1)*TYPE_W-1 -: TYPE_W]] <= count[k_nearest_neighbours_type[(j+1)*TYPE_W-1 -: TYPE_W]] + 1;
               j <= j + 1;
             end else begin
               j <= 0;
@@ -45,7 +49,7 @@ module k_type #(K, TYPE_W)(
             end
           end else begin
             if (j < (1 << TYPE_W)) begin
-              if (count[j] > count[max_type]) max_type <= j;
+              if (count[32*(j+1)-1-:32] > count[32*(max_type+1)-1-:32]) max_type <= j;
               j <= j + 1;
             end else begin
               state <= 2'b10;
